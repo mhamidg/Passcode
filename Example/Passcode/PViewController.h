6344ad925cad893440ff6409acdffca621f0696e
Hamid <mhamidg@gmail.com>
2022-07-20 10:56:17 +0400

    Initial commit

---
+++ Example/Passcode/PViewController.h
@@ -0,0 +1,13 @@
+//
+//  PViewController.h
+//  Passcode
+//
+//  Created by mhamidg on 07/20/2022.
+//  Copyright (c) 2022 mhamidg. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PViewController : UIViewController
+
+@end
---
+++ Example/Passcode/PViewController.m
@@ -0,0 +1,29 @@
+//
+//  PViewController.m
+//  Passcode
+//
+//  Created by mhamidg on 07/20/2022.
+//  Copyright (c) 2022 mhamidg. All rights reserved.
+//
+
+#import "PViewController.h"
+
+@interface PViewController ()
+
+@end
+
+@implementation PViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
